34ba4dd0f8bcb0aa63e576c91656fb33c4d4c800
Danny André Keim <dannyandre.k2@gso.schule.koeln>
2022-09-01 13:10:01 +0200

    UD

--- 2EingabeAusgabe/Program.cs
+++ 2EingabeAusgabe/Program.cs
@@ -1,2 +1,22 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+//Aufgabe 1
+Console.WriteLine("  .-. \n (o o) \n | O | \n |   | \n '~~~'");
+
+//Aufgabe 2
+
+string Name = "Danny";
+
+string Schule = "GSO";
+
+int Alter = 16;
+
+Console.WriteLine(Name);
+
+Console.WriteLine(Schule);
+
+Console.WriteLine(Alter);
+
+//Aufgabe 3
